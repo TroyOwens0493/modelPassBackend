f5adff6a989c0c3de32ead78a2e2595c16c6664e
nathanesky <esc21007@byui.edu>
2026-06-23 10:54:33 -0600

    feat: add WorkOS AuthKit integration for node

---
+++ dist/workos.d.ts
@@ -0,0 +1,5 @@
+import { WorkOS } from "@workos-inc/node";
+export declare const workos: WorkOS;
+export declare const clientId: string;
+export declare const redirectUri: string;
+export declare const cookiePassword: string | undefined;
