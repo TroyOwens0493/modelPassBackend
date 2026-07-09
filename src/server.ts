003bd9c0b1a33f116f03074c7bae9a93a9d77b2e
TroyOwens0493 <147565176+TroyOwens0493@users.noreply.github.com>
2026-07-09 10:08:21 -0600

    Add shared MongoDB connection

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
-import { MongoClient } from "mongodb";
 import { app } from "./app.js";
+import { connectDb } from "./db.js";
 
 const PORT = process.env.PORT || 3000;
 
-const client = new MongoClient(process.env.MONGODB_URI!);
-await client.connect();
+await connectDb();
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
